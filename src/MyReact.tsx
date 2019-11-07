c61e5c43741aed30824b3c60c0b7d22512470f91
QuantoC <cmat@optonline.net>
2019-11-06 20:27:15 -0500

    Removed comments

--- src/MyReact.tsx
+++ src/MyReact.tsx
@@ -1,52 +1,6 @@
-Skip to content
-
-
-  
-Pull requests 
-Issues 
-Marketplace 
-Explore 
-
-
- 
-
-
-
-This repository has been archived by the owner. It is now read-only. 
-Watch 
-185 
-Star 
-9.7k 
-Fork 
-951 
-
-microsoft/TypeScript-React-Starter Archived 
-Code 
-Issues 120 
-Pull requests 83 
-Actions 
-Projects 0 
-Wiki 
-Security 
-Insights 
-Branch: master 
-TypeScript-React-Starter/src/components/Hello.tsx 
-Find file 
-Copy path 
- bowdenk7 initial commit 
-f9fbbdd on May 9, 2017 
-1 contributor 
-36 lines (29 sloc) 780 Bytes 
-Raw
-Blame
-History
-
 
 import * as React from 'react';
-
 import './Hello.css';
-
-
 
 export interface Props {
 
